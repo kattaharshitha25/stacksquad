5b7aa507be022de3a50d45b1e31c9cabfd6c7123
kattaharshitha25 <kataharshitha2021@gmail.com>
2025-07-31 00:49:22 +0530

    added mapculturesjs

--- JAVASCRIPT_code/mapjvs.js
+++ JAVASCRIPT_code/mapjvs.js
@@ -11,246 +11,108 @@ map.setMaxBounds([
   [35.67, 97.395] 
 ]);
 
-const icon1 = L.icon({
-  iconUrl: 'mapimages/warli-icon.png',
-  iconSize: [52, 52]
-})
-const icon2 = L.icon({
-  iconUrl: 'mapimages/kalighat.png',
-  iconSize: [52, 52]
-})
-const icon3 = L.icon({
-  iconUrl: 'mapimages/kalamk.png',
-  iconSize: [120, 120]
-})
-const icon4 = L.icon({
-  iconUrl: 'mapimages/madhubani-br.png',
-  iconSize: [92, 92]
-})
-const icon5 = L.icon({
-  iconUrl: 'mapimages/pattachitra.png',
-  iconSize: [92, 92]
-})
-const icon6 = L.icon({
-  iconUrl: 'mapimages/tanjore.png',
-  iconSize: [92, 92]
-})
-const icon7 = L.icon({
-  iconUrl: 'mapimages/chitrakathi.png',
-  iconSize: [92, 92]
-})
-const icon8 = L.icon({
-  iconUrl: 'mapimages/gondpain.png',
-  iconSize: [102, 132]
-})
-const icon9 = L.icon({
-  iconUrl: 'mapimages/dollguj.png',
-  iconSize: [72, 92]
-})
-const icon10 = L.icon({
-  iconUrl: 'mapimages/phadraj.png',
-  iconSize: [72, 92]
-})
-const icon11 = L.icon({
-  iconUrl: 'mapimages/sanjhiup.png',
-  iconSize: [62, 62]
-})
-const icon12 = L.icon({
-  iconUrl: 'mapimages/punjab.png',
-  iconSize: [72, 82]
-})
-const icon13 = L.icon({
-  iconUrl: 'mapimages/kashmirarts.png',
-  iconSize: [62, 72]
-})
-const icon14 = L.icon({
-  iconUrl: 'mapimages/assamtoys.png',
-  iconSize: [62, 72]
-})
-const icon15 = L.icon({
-  iconUrl: 'mapimages/manipur.png',
-  iconSize: [62, 72]
-})
-const icon16 = L.icon({
-  iconUrl: 'mapimages/arunap.png',
-  iconSize: [62, 72]
-})
+const icon1 = L.icon({ 
+  iconUrl: '../pictures/mapimages/kalamk.png',
+  iconSize: [120, 120] });
+const icon2 = L.icon({ 
+  iconUrl: '../pictures/mapimages/arunap.png', 
+  iconSize: [62, 72] });
+const icon3 = L.icon({ 
+  iconUrl: '../pictures/mapimages/assamtoys.png', 
+  iconSize: [62, 72] });
+const icon4 = L.icon({ 
+  iconUrl: '../pictures/mapimages/madhubani-br.png', 
+  iconSize: [92, 92] });
+const icon5 = L.icon({ 
+  iconUrl: '../pictures/mapimages/dollguj.png', 
+  iconSize: [72, 92] });
+const icon6 = L.icon({ 
+  iconUrl: '../pictures/mapimages/kashmirarts.png', 
+  iconSize: [62, 72] });
+const icon7 = L.icon({ 
+  iconUrl: '../pictures/mapimages/chitrakathi.png', 
+  iconSize: [152, 92] });
+const icon8 = L.icon({ 
+  iconUrl: '../pictures/mapimages/gondpain.png', 
+  iconSize: [102, 132] });
+const icon9 = L.icon({ 
+  iconUrl: '../pictures/mapimages/warli-icon.png', 
+  iconSize: [52, 52] });
+const icon10 = L.icon({ 
+  iconUrl: '../pictures/mapimages/manipur.png', 
+  iconSize: [62, 72] });
+const icon11 = L.icon({ 
+  iconUrl: '../pictures/mapimages/pattachitra.png', 
+  iconSize: [92, 92] });
+const icon12 = L.icon({ 
+  iconUrl: '../pictures/mapimages/punjab.png', 
+  iconSize: [72, 82] });
+const icon13 = L.icon({ 
+  iconUrl: '../pictures/mapimages/phadraj.png', 
+  iconSize: [72, 92] });
+const icon14 = L.icon({ 
+  iconUrl: '../pictures/mapimages/tanjore.png', 
+  iconSize: [92, 92] });
+const icon15 = L.icon({ 
+  iconUrl: '../pictures/mapimages/sanjhiup.png', 
+  iconSize: [62, 62] });
+const icon16 = L.icon({ 
+  iconUrl: '../Pictures/mapimages/kalighat.png', 
+  iconSize: [52, 52] });
 
-
-const marker1 = L.marker([15.9129, 79.73999],{
-  icon: icon3
-});
-const marker2 = L.marker([28.2180, 94.7278], {
-  icon: icon16
-});
-const marker3 = L.marker([26.2006, 89.9376], {
-  icon: icon14
-});
-const marker4 = L.marker([26.0961, 85.3131],{
-  icon: icon4
-});
-const marker7 = L.marker([22.2587, 71.1924], {
-  icon: icon9
-});
-const marker9 = L.marker([34.6048, 76.1734], {
-  icon: icon13
-});
-const marker11 = L.marker([15.3173, 75.7139], {
-  icon: icon7
-});
-const marker13 = L.marker([22.9734, 78.6569], {
-  icon: icon8
-});
-const marker14 = L.marker([19.7515, 75.7139], {
-  icon: icon1
-});
-const marker15 = L.marker([24.6637, 93.9063], {
-  icon: icon15
-});
-const marker19 = L.marker([20.0451, 83.0985], {
-  icon: icon5
-});
-const marker20 = L.marker([30.9471, 76.3412], {
-  icon: icon12
-});
-const marker21 = L.marker([27.0238, 74.2179], {
-  icon: icon10
-});
-const marker23 = L.marker([11.1271, 78.6569],{
-  icon: icon6
-});
-const marker26 = L.marker([27.8467, 80.9462], {
-  icon: icon11
-});
-const marker28 = L.marker([22.9868, 87.8550], {
-  icon: icon2
-});
+const marker1 = L.marker([15.9129, 79.73999], { icon: icon1 });
+const marker2 = L.marker([28.2180, 94.7278], { icon: icon2 });
+const marker3 = L.marker([26.2006, 89.9376], { icon: icon3 });
+const marker4 = L.marker([26.0961, 85.3131], { icon: icon4 });
+const marker5 = L.marker([22.2587, 71.1924], { icon: icon5 });
+const marker6 = L.marker([34.6048, 76.1734], { icon: icon6 });
+const marker7 = L.marker([15.3173, 75.7139], { icon: icon7 });
+const marker8 = L.marker([22.9734, 78.6569], { icon: icon8 });
+const marker9 = L.marker([19.7515, 75.7139], { icon: icon9 });
+const marker10 = L.marker([24.6637, 93.9063], { icon: icon10 });
+const marker11 = L.marker([20.0451, 83.0985], { icon: icon11 });
+const marker12 = L.marker([30.9471, 76.3412], { icon: icon12 });
+const marker13 = L.marker([27.0238, 74.2179], { icon: icon13 });
+const marker14 = L.marker([11.1271, 78.6569], { icon: icon14 });
+const marker15 = L.marker([27.8467, 80.9462], { icon: icon15 });
+const marker16 = L.marker([22.9868, 87.8550], { icon: icon16 });
 
 marker1.addTo(map);
 marker2.addTo(map);
 marker3.addTo(map);
 marker4.addTo(map);
+marker5.addTo(map);
+marker6.addTo(map);
 marker7.addTo(map);
+marker8.addTo(map);
 marker9.addTo(map);
+marker10.addTo(map);
 marker11.addTo(map);
+marker12.addTo(map);
 marker13.addTo(map);
 marker14.addTo(map);
 marker15.addTo(map);
-marker19.addTo(map);
-marker20.addTo(map);
-marker21.addTo(map);
-marker23.addTo(map);
-marker26.addTo(map);
-marker28.addTo(map);
+marker16.addTo(map);
 
-marker1.bindPopup(`
-  <strong>State:</strong> Andhra Pradesh<br>
-  <strong>Famous Art:</strong> Kalamkari<br>
-  <strong>Famous Artist:</strong> Jonnalagadda Gurappa Chetty<br>
-  <strong>Famous Museum:</strong> Salar Jung Museum, Hyderabad
-`);
 
-marker2.bindPopup(`
-  <strong>State:</strong> Arunachal Pradesh<br>
-  <strong>Famous Art:</strong> Monpa wood carving and Thangka painting<br>
-  <strong>Famous Artist:</strong> Padmashree Tsering Norbu<br>
-  <strong>Famous Museum:</strong> Jawaharlal Nehru State Museum, Itanagar
-`);
+function generateList(){
+  const ul = document.querySelector('.list');
+  culturelist.forEach((state) => { 
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const a = document.createElement('a');
+    const p = document.createElement('p');
 
-marker3.bindPopup(`
-  <strong>State:</strong> Assam<br>
-  <strong>Famous Art:</strong> Toy and mask making of Majuli<br>
-  <strong>Famous Artist:</strong> Hem Chandra Goswami<br>
-  <strong>Famous Museum:</strong> Assam State Museum, Guwahati
-`);
+    div.classList.add('cultures-list');
+    a.innerText = state.properties.name;
+    a.href = '#';
+    a.innerText = state.properties.origin;
 
-marker4.bindPopup(`
-  <strong>State:</strong> Bihar<br>
-  <strong>Famous Art:</strong> Madhubani Painting<br>
-  <strong>Famous Artist:</strong> Sita Devi<br>
-  <strong>Famous Museum:</strong> Bihar Museum, Patna
-`);
+    div.appendChild(a);
+    div.appendChild(p);
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
+}
 
-marker7.bindPopup(`
-  <strong>State:</strong> Gujarat<br>
-  <strong>Famous Art:</strong> Clay Dolls of Kutch<br>
-  <strong>Famous Artist:</strong> Haribhai Kumbhar<br>
-  <strong>Famous Museum:</strong> Kutch Museum, Bhuj
-`);
-
-marker9.bindPopup(`
-  <strong>State:</strong> Jammu & Kashmir<br>
-  <strong>Famous Art:</strong> Papier-mâché and Sozni embroidery<br>
-  <strong>Famous Artist:</strong> Ghulam Rasool Khan<br>
-  <strong>Famous Museum:</strong> SPS Museum, Srinagar
-`);
-
-marker11.bindPopup(`
-  <strong>State:</strong> Karnataka<br>
-  <strong>Famous Art:</strong> Chitrakathi and Leather Puppetry<br>
-  <strong>Famous Artist:</strong> K.V. Raghavaiah<br>
-  <strong>Famous Museum:</strong> Government Museum, Bengaluru
-`);
-
-marker13.bindPopup(`
-  <strong>State:</strong> Madhya Pradesh<br>
-  <strong>Famous Art:</strong> Gond Painting<br>
-  <strong>Famous Artist:</strong> Jangarh Singh Shyam<br>
-  <strong>Famous Museum:</strong> Bharat Bhavan, Bhopal
-`);
-
-marker14.bindPopup(`
-  <strong>State:</strong> Maharashtra<br>
-  <strong>Famous Art:</strong> Warli Painting<br>
-  <strong>Famous Artist:</strong> Jivya Soma Mashe<br>
-  <strong>Famous Museum:</strong> Chhatrapati Shivaji Maharaj Vastu Sangrahalaya, Mumbai
-`);
-
-marker15.bindPopup(`
-  <strong>State:</strong> Manipur<br>
-  <strong>Famous Art:</strong> Manipuri Doll-making and Textiles<br>
-  <strong>Famous Artist:</strong> Lourembam Tombi<br>
-  <strong>Famous Museum:</strong> Manipur State Museum, Imphal
-`);
-
-marker19.bindPopup(`
-  <strong>State:</strong> Odisha<br>
-  <strong>Famous Art:</strong> Pattachitra<br>
-  <strong>Famous Artist:</strong> Ananta Maharana<br>
-  <strong>Famous Museum:</strong> Odisha State Museum, Bhubaneswar
-`);
-
-marker20.bindPopup(`
-  <strong>State:</strong> Punjab<br>
-  <strong>Famous Art:</strong> Phulkari<br>
-  <strong>Famous Artist:</strong> Harnam Kaur<br>
-  <strong>Famous Museum:</strong> Virasat-e-Khalsa, Anandpur Sahib
-`);
-
-marker21.bindPopup(`
-  <strong>State:</strong> Rajasthan<br>
-  <strong>Famous Art:</strong> Phad Painting<br>
-  <strong>Famous Artist:</strong> Shree Lal Joshi<br>
-  <strong>Famous Museum:</strong> Albert Hall Museum, Jaipur
-`);
-
-marker23.bindPopup(`
-  <strong>State:</strong> Tamil Nadu<br>
-  <strong>Famous Art:</strong> Tanjore Painting and Chola Bronzes<br>
-  <strong>Famous Artist:</strong> M. Singaravelu<br>
-  <strong>Famous Museum:</strong> Government Museum, Chennai
-`);
-
-marker26.bindPopup(`
-  <strong>State:</strong> Uttar Pradesh<br>
-  <strong>Famous Art:</strong> Sanjhi Art<br>
-  <strong>Famous Artist:</strong> Ram Soni<br>
-  <strong>Famous Museum:</strong> Bharat Kala Bhavan, Varanasi
-`);
-
-marker28.bindPopup(`
-  <strong>State:</strong> West Bengal<br>
-  <strong>Famous Art:</strong> Kalighat Painting<br>
-  <strong>Famous Artist:</strong> Jamini Roy<br>
-  <strong>Famous Museum:</strong> Indian Museum, Kolkata
-`);
+generateList();
